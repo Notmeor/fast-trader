98efc22fee9278a98160ca732298fb39304dbbc8
xiongjianqiang <xiongjianqiang@idwzx.com>
2018-10-16 15:24:22 +0800

    Add files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fast_trader',
+    version='0.1.0',
+    packages=find_packages(include=["fast_trader"]),
+    author='notmeor',
+    author_email='kevin.inova@gmail.com',
+    description='',
+    install_requires=[]
+)
+
