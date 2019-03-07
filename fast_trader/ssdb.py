fe75dd3a0d7f065fd04f3a1f71f6fc9df68870c5
xiongjianqiang <xiongjianqiang@idwzx.com>
2019-03-07 10:30:21 +0800

    SSDBListStore now does background buffered writing

--- fast_trader/ssdb.py
+++ fast_trader/ssdb.py
@@ -1,3 +1,9 @@
+
+import datetime
+import collections
+import threading
+import queue
+
 from fast_trader.dtp_quote import conf
 from fast_trader.serializer import serializer 
 
@@ -42,17 +48,64 @@ class SSDBListStore:
     key naming: 'category;code;date'
     """
 
-    def __init__(self, *args, **kw):
+    def __init__(self):
 
         self._client = get_client()
+        self._write_buffer_size = conf['quote_feed_store']['ssdb_write_buffer_size']
+        self._buffer = queue.Queue()
+        self._writer = None 
+        self._buffer_interval = 10
+        self._last_write_time = datetime.datetime.now()
+        self._writer_running = True
+        self._buffer_write()
 
     @property
     def client(self):
         return self._client
     
-    def push(self, key, value):
+    def _push(self, key, value):
         b = serializer.serialize(value)
         self.client.qpush(key, b)
+    
+    def _push_many(self, key, values, client=None):
+        many = [serializer.serialize(v) for v in values]
+        if client is None:
+            cl = self.client
+        else:
+            cl = client
+        cl.qpush(key, *many)
+    
+    def _buffer_write(self):
+        if self._write_buffer_size <= 0:
+            return
+        
+        def _flush():
+            client = get_client()
+            buf = collections.defaultdict(list)
+            while self._writer_running:
+                now = datetime.datetime.now()
+                if (now - self._last_write_time).seconds > self._buffer_interval:
+                    slef._last_write_time = now
+                    try:
+                        for i in range(self._buffer.qsize()):
+                            el = self._buffer.get(block=False)
+                            buf[el[0]].append(el[1])
+                    except queue.Empty:
+                        pass
+                    for key in buf:
+                        v = buf[key]
+                        if len(v) > 0:
+                            self._push_many(key, v, client)
+                            buf[key] = []
+        
+        self._writer = threading.Thread(target=_flush)
+        self._writer.start()
+
+    def push(self, key, value):
+        if self._write_buffer_size == 0:
+            self._push(key, value)
+        else:
+            self._buffer.put((key, value))
 
     def read_first(self, key):
         ret = self.client.qfront(key)
